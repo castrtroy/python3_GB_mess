e6ca441a2d92056dc642ca3051f9f4aba421cd9e
Aleksei Varlamov <castrtroy@gmail.com>
2018-05-20 19:17:02 +0300

    исправил порт

--- client.py
+++ client.py
@@ -2,13 +2,14 @@ from socket import *
 #Импортировал библиотеку
 s = socket(AF_INET,SOCK_STREAM)
 host = 'localhost'
-port = 8888
+port = 7777
 addr = (host, port)
 #Переменный для удобства
 s.connect(addr)
 #Соединение с сервером
 tm = s.recv(1024)
 #количество байт
+
+print ("Текущее время: %s" % tm.decode('ascii'))
 s.close()
 #Закрываем соединенеие
-print ("Текущее время: %s" % tm.decode('ascii'))
